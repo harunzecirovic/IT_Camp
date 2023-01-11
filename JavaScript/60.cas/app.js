9604f2e8b899bdb29420df9e5934705def23a5ac
Harun <harun.zecirovic@gmail.com>
2023-01-11 20:59:08 +0100

    60. i 61. cas

---
+++ JavaScript/60.cas/app.js
@@ -0,0 +1,23 @@
+// document.getElementById('someID')
+// document.getElementsByClassName('someClass')
+// document.getElementsByTagName('tagName')
+// document.querySelector('someID or .className')
+
+// document.createElement('p')
+// document.removeChild('p')
+// document.appendChild('p')
+// document.replaceChild('new','old')
+// document.write('text')
+
+// document.getElementById('id').on = function () {
+//     // some fn
+// }
+
+
+// create a to-do 
+
+var ran = document.getElementById('ran')
+
+ran.addEventListener('input', (input) => {
+    console.log(input)
+})
