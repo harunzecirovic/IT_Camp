60ca506b2da720cbe2e90c6175fcf0c7685ac45a
Harun <harun.zecirovic@gmail.com>
2023-01-18 21:00:52 +0100

    65.cas

--- React-nitshop/src/App.jsx
+++ React-nitshop/src/App.jsx
@@ -1,14 +1,27 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StyledButton from './components/styleButton/styledButton';
 import StyledHeader from './components/styleButton/styledHeader'
+
 const App =() => {
+  const[innerText, setInnerText] = useState('')
+  const[clicked,setClicked] = useState(false)
+
+  useEffect(() => {
+    clicked ? setInnerText('Clicked') : setInnerText('Click me')
+  }, [clicked])
+
+  function buttonClick() {
+    setClicked(!clicked)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <StyledHeader innerText="This is header"/>
-        <StyledButton innerText="CLICK"/>
+        <StyledButton innerText={innerText} onClickHandler={buttonClick}/>
         <a
           className="App-link"
           href="https://reactjs.org"
