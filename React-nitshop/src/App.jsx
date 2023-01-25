9ab4beb8435a294210d43632e7222f00685e6edf
Harun <harun.zecirovic@gmail.com>
2023-01-25 20:59:57 +0100

    66.cas

--- React-nitshop/src/App.jsx
+++ React-nitshop/src/App.jsx
@@ -1,26 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import StyledForm from './components/StyledForm/StyledForm';
-import CustomDiv from './components/CustomDiv/CustomDiv';
+import ToDo from './components/ToDo/ToDo';
 
-const App = () =>{
-  const [fullName, setFullName] = useState('')
-
-  function dataGraber(name,lastName) {
-    let fullName = name + ' ' + lastName
-    console.log(fullName)
-    setFullName(fullName)
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <CustomDiv>
-          <h1>{fullName}</h1>
-        </CustomDiv>
-        <StyledForm sendData={(name, lastName) => dataGraber(name,lastName)} />
-      </header>
-    </div>
-  );
-} 
+const App = () => {
+  return(
+    <>
+    <ToDo />
+    </>
+  )
+}
 
 export default App;
